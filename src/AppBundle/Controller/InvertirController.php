5eac52d80e1713516e1ee8239bf6fab9042494f6
zinho <zinho@localhost>
2017-11-03 23:31:41 -0300

    Consalud

--- src/AppBundle/Controller/InvertirController.php
+++ src/AppBundle/Controller/InvertirController.php
@@ -7,9 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Response;
-use BackendBundle\Entity\Usuario;
-use AppBundle\Form\RegisterType;
-use AppBundle\Form\UserType;
+use BackendBundle\Entity\Ctacte;
+use BackendBundle\Entity\Proyectos;
+use BackendBundle\Entity\Movimientos;
 
 class InvertirController extends Controller {
 
@@ -19,7 +19,29 @@ class InvertirController extends Controller {
         $this->session = new Session();
     }
     public function invertirAction(Request $request,$id=null) {
+        $em= $this->getDoctrine()->getManager();
+        $compra=new Movimientos();
+        $form = $this->createForm('AppBundle\Form\MovimientosType', $compra);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($usuario);
+            $flush = $em->flush();
+            $editLink = $this->generateUrl('proyectos_edit', array('id' => $usuarios->getIdUsuario()));
+            if ($flush == null) {
+                $this->get('session')->getFlashBag()->add('success', "<a href='$editLink'>proyecto creado .</a>");
+            } else {
+                $this->get('session')->getFlashBag()->add('danger', "<a href='$editLink'>Proyecto NO  creado .</a>");
+            }
+        }
+
+        $ctacte_repo=$em->getRepository("BackendBundle:Ctacte");
+        $movimientos_Repo="";
+        
+        $proyecto_repo=$em->getRepository("BackendBundle:Proyectos")->findOneBy(array('id' => $id));
         return $this->render('AppBundle:Invertir:index.html.twig', array(
+            'proyecto'      =>  $proyecto_repo, 
+            'movimiento'    =>  $form->createView()
         ));
         
     }
