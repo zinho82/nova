93396688259ec3877dbb79d9227621bf05bd4bed
zinho <zinho@localhost>
2017-11-05 02:19:05 -0300

    ctacte depositos

--- src/AppBundle/Controller/InvertirController.php
+++ src/AppBundle/Controller/InvertirController.php
@@ -21,46 +21,55 @@ class InvertirController extends Controller {
 
     public function invertirAction(Request $request, Proyectos $proyectos) {
         $acciones = 0;
+        $apago = 0;
         $user = $this->getUser();
-
+        $idu = $user->getIdusuario();
         $em = $this->getDoctrine()->getManager();
         $compra = new Movimientos();
         $form = $this->createForm('AppBundle\Form\MovimientosType', $compra);
         $ctacte_repo = $em->createQuery(
-                "select sum(c.monto) from BackendBundle:Ctacte c where c.usuario_idusuario=$user" )->getResult();
+                        "select sum(c.monto)  from BackendBundle:Ctacte c where c.usuariousuario=$idu and c.estado=3")->setMaxResults(1)->getOneOrNullResult();
 
-//        $proyecto_repo = $em->getRepository("BackendBundle:Proyectos")->findOneBy(array('id' => $id));
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $acciones = $form->get('acciones')->getData();
             $moviemiento = $form->get('tipoaccion')->getData();
-            if ($moviemiento == 'Compra') {
-                if ($compra->getAcciones() <= $acciones) {
+            if ($acciones > 0) {
+                if ($moviemiento == 'Compra') {
+                    if ($compra->getAcciones() <= $acciones) {
+                        $apago = $compra->getAcciones() * ($proyectos->getValor() + $proyectos->getCop());
+                        if ($ctacte_repo[1] >= $apago) {
+                            $compra->setReaded(0);
+                            $compra->setProyectos($proyectos);
+                            $compra->setUsrdestino($user);
+                            $compra->setUsuariousuario($user);
+                            $compra->setEstado(1);
+                            $em->persist($compra);
+                            $flush = $em->flush();
 
-                    $compra->setReaded(0);
-                    $compra->setProyectos($proyectos);
-                    $compra->setUsrdestino($user);
-                    $compra->setUsuariousuario($user);
-                    $compra->setEstado(1);
-                    $em->persist($compra);
-                    $flush = $em->flush();
-                    
-                    if ($flush == null) {
-                        $this->get('session')->getFlashBag()->add('success', "Acciones Compradas");
+                            if ($flush == null) {
+                                $this->get('session')->getFlashBag()->add('success', "Acciones Compradas");
+                            } else {
+                                $this->get('session')->getFlashBag()->add('danger', ">Acciones NO  Compradas");
+                            }
+                        } else {
+                            $this->get('session')->getFlashBag()->add('danger', "La Cantidad de Acciones Excede su monto disponible");
+                        }
                     } else {
-                        $this->get('session')->getFlashBag()->add('danger', ">Acciones NO  Compradas");
+                        $this->get('session')->getFlashBag()->add('danger', "La Cantidad de acciones a comprar, es mayor que las acciones disponibles");
                     }
-                } else {
-                    $this->get('session')->getFlashBag()->add('danger', "La Cantidad de acciones a comprar, es mayor que las acciones disponibles");
                 }
-            } 
+            }else{
+                $this->get('session')->getFlashBag()->add('danger', "Debe al menos transar 1 accion");
+            }
         }
 
         return $this->render('AppBundle:Invertir:index.html.twig', array(
                     'proyecto' => $proyectos,
                     'form' => $form->createView(),
                     'acciones' => $acciones,
-            'monto' => $ctacte_repo,
+                    'monto' => $ctacte_repo,
+                    'pagar' => $apago
         ));
     }
 
--- src/AppBundle/Services/NotificationService.php
+++ src/AppBundle/Services/NotificationService.php
@@ -21,11 +21,11 @@ class NotificationService {
                 
     }
     
-    public function set($codProy,$stado,$user,$acciones) {
+    public function set($codProy,$stado,$user) {
         $em= $this->manager;
         $notification=new Registro();
         $notification->setIdusuario($user);
-        $notification->setAcciones($acciones);
+        $notification->setAcciones(0);
         $notification->setReaded(0);
         $notification->setEstado($stado);
         $notification->setCodigoproyecto($codProy);
